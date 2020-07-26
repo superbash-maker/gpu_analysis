c09f6f9b34069ee9a5cf2068c341a039d1c54e72
superbash-maker <kk.govender@gmail.com>
2020-07-26 21:36:48 +0200

    Changed the way date is formatted

--- process_logs_gpu2001_0.py
+++ process_logs_gpu2001_0.py
@@ -10,22 +10,24 @@ os.remove("code_usage_gpu2001_0.csv")
 
 # Create file to write to:
 fu = open("user_usage_gpu2001_0.csv","a")
-fu.write("Date"+","+"GPUID"+","+"Username"+","+"Percent"+"\n")
+fu.write("Date"+","+"GPUID"+","+"Username"+","+"Percent_Usage"+"\n")
 # Create file to write to:
 fc = open("code_usage_gpu2001_0.csv","a")
-fc.write("Date"+","+"GPUID"+","+"Code"+","+"Percent"+"\n")
+fc.write("Date"+","+"GPUID"+","+"Code"+","+"Percent_Usage"+"\n")
 
 #This will cycle through dates from 27 July 2019 - Present
 form=np.array([])
-start_date = date(2020, 7, 19)
+form2=np.array([])
+start_date = date(2020, 7, 1)
 end_date = date(2020, 7, 21)
 daterange = pd.date_range(start_date, end_date)
 for single_day in daterange:
     form=np.append(form, single_day.strftime("%Y%m%d"))
+    form2=np.append(form2, single_day.strftime("%Y-%m-%d"))
 
 for day in range(len(form)):
     # Extract the raw data and store them in arrays
-    alldata=np.char.strip(np.loadtxt("/home/osboxes/Documents/gpu_usage/logs/gpu/gpu2001/"+form[day], dtype='str', delimiter=","))
+    alldata=np.char.strip(np.loadtxt("logs/gpu/gpu2001/"+form[day], dtype='str', delimiter=","))
     datetime=alldata[0:,0]
     gpunode=alldata[0:,1]
     app=alldata[0:,8]
@@ -42,7 +44,7 @@ for day in range(len(form)):
         if ((gpunum[i] == "0")):
             codegpu=np.append(codegpu,app[i])
             usergpu=np.append(usergpu,username[i])
-            date=np.append(date,form[day])
+            date=np.append(date,form2[day])
     userid=pd.Series(usergpu).value_counts().keys().tolist()
     countu=pd.Series(usergpu).value_counts().tolist()
     codeid=pd.Series(codegpu).value_counts().keys().tolist()
