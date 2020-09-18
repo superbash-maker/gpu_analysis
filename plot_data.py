6857c5461d4dec88d818b7e705be4d401555a1f2
superbash-maker <kk.govender@gmail.com>
2020-09-18 10:44:45 +0200

    Remove unused python script

--- plot_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-df = pd.read_csv('user_usage_gpu2001_0.csv')
-fig = px.scatter(df, x = 'Date', y = 'Percent_Usage', title='GPU2001_ID_0', text='Username')
-fig.update_traces(textposition='top center')
-fig.show()
